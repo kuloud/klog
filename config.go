27fc962cbca644ea04d235c7d615a332f8b09e1f
肖中中 <kuloud@ninakanakas-MacBook-Pro.local>
2015-11-07 22:08:07 +0800

    Init the project.

---
+++ config.go
@@ -0,0 +1,7 @@
+package klog
+
+const (
+	FILE_LOG_ENABLE    = true
+	FILE_LOG_MAX_LINES = 50000
+	FILE_LOG_MAX_SIZE  = 5000000
+)
---
+++ log_test.go
@@ -0,0 +1,52 @@
+package klog
+
+import (
+	"testing"
+)
+
+func TestV(t *testing.T) {
+	V("kuloud", "TestV", "SUCCESS")
+}
+
+func BenchmarkV(b *testing.B) {
+	for i := 1; i < b.N; i++ {
+		D("Benchmark", "TestV", "SUCCESS", i)
+	}
+
+}
+
+func TestVf(t *testing.T) {
+	Vf("tag", "%s", "Hello")
+}
+
+func TestD(t *testing.T) {
+	D("tag", "Hello")
+}
+
+func TestDf(t *testing.T) {
+	Df("tag", "%s", "Hello")
+}
+
+func TestI(t *testing.T) {
+	I("tag", "Hello")
+}
+
+func TestIf(t *testing.T) {
+	If("tag", "Hello = %s%s", "Kuloud", "dddd")
+}
+
+func TestW(t *testing.T) {
+	W("tag", "Hello")
+}
+
+func TestWf(t *testing.T) {
+	Wf("tag", "%s%s", "Kuloud", "kkk")
+}
+
+func TestE(t *testing.T) {
+	E("tag", "Hello")
+}
+
+func TestEf(t *testing.T) {
+	Ef("tag", "%s", "Hello")
+}
